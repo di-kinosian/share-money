d8f6d0776f7397a20ebcc16acc4c1dae3973e119
Diana Kinosian <kinosyan.diana@gmail.com>
2021-08-20 14:37:34 +0300

    add-transaction-payload

--- src/views/Balance/NewTransactionWidget/index.jsx
+++ src/views/Balance/NewTransactionWidget/index.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState } from 'react';
 import {
 	NewTransactionBlock,
 	CloseButton,
@@ -8,7 +9,6 @@ import {
 	TracsactionInput,
 	RowFields,
 	AmountInput,
-	DateInput,
 	PayerInput,
 	UserAmountRow,
 	UserName,
@@ -19,8 +19,68 @@ import {
 import closeIcon from '../../../assets/img/close-icon.svg';
 import { DATE_FORMAT } from '../../../modules/special/constants';
 import { Icon } from 'semantic-ui-react';
+import { DateInput } from 'semantic-ui-calendar-react';
+import { v4 as uuid } from 'uuid';
+import moment from 'moment';
 
 function NewTransactionWidget(props) {
+	const [spentUsers, setSpentUsers] = useState(
+		props.users.map((user) => {
+			return { ...user, amount: '0.00' };
+		})
+	);
+	const [paidUsers, setPaidUsers] = useState(
+		props.users.map((user) => {
+			return { ...user, amount: '0.00' };
+		})
+	);
+	const [titleInput, setTitleInput] = useState('');
+	const [amountInput, setAmountInput] = useState('0.00');
+	const [dataInput, setDataInput] = useState(moment().format(DATE_FORMAT));
+
+	const createObj = () => {
+		console.log({
+			title: titleInput,
+			id: uuid(),
+			amount: amountInput,
+			date: dataInput,
+			paidUsers: paidUsers,
+			spentUsers: spentUsers,
+		});
+	};
+
+	const enterInTitleInput = (event) => {
+		setTitleInput(event.target.value);
+	};
+
+	const changeAmountInput = (event) => {
+		setAmountInput(event.target.value);
+	};
+
+	const handleDateChange = (event, data) => {
+		setDataInput(data.value);
+	};
+
+	const changePaidAmount = (event) => {
+		let userId = event.target.dataset.id;
+		const newPaidUsers = paidUsers.map((user) => {
+			return user.id === userId
+				? { ...user, amount: event.target.value }
+				: user;
+		});
+		setPaidUsers(newPaidUsers);
+	};
+
+	const changeSpentAmount = (event) => {
+		let userId = event.target.dataset.id;
+		const newSpentUsers = spentUsers.map((user) => {
+			return user.id === userId
+				? { ...user, amount: event.target.value }
+				: user;
+		});
+		setSpentUsers(newSpentUsers);
+	};
+
 	return (
 		<NewTransactionBlock>
 			<CloseButton /*onClick={}*/>
@@ -28,12 +88,18 @@ function NewTransactionWidget(props) {
 			</CloseButton>
 			<TracsactionField>
 				<Text>Title:</Text>
-				<TracsactionInput placeholder="Enter title" />
+				<TracsactionInput
+					placeholder="Enter title"
+					value={titleInput}
+					onChange={enterInTitleInput}
+				/>
 			</TracsactionField>
 			<RowFields>
 				<TracsactionField>
 					<Text>Amount:</Text>
 					<AmountInput
+						value={amountInput}
+						onChange={changeAmountInput}
 						min={0}
 						type="number"
 						id="amount-input"
@@ -52,26 +118,29 @@ function NewTransactionWidget(props) {
 						animation="scale"
 						duration={200}
 						hideMobileKeyboard
-						// value={date}
+						value={dataInput}
 						iconPosition="left"
 						preserveViewMode={false}
 						autoComplete="off"
-						// onChange={handleDateChange}
+						onChange={handleDateChange}
 					></DateInput>
 				</TracsactionField>
 			</RowFields>
 
 			<Text>Paid:</Text>
 			<TracsactionField>
-				{props.users.map((item) => {
+				{paidUsers.map((user) => {
 					return (
-						<UserAmountRow key={'1'}>
-							<UserName>{item.name}</UserName>
+						<UserAmountRow key={user.id}>
+							<UserName>{user.name}</UserName>
 							<PayerInput
 								min={0}
+								value={user.amount}
 								type="number"
 								id="amount-input"
 								className="amount-input"
+								onChange={changePaidAmount}
+								data-id={user.id}
 							></PayerInput>
 						</UserAmountRow>
 					);
@@ -80,15 +149,18 @@ function NewTransactionWidget(props) {
 
 			<Text>Spent:</Text>
 			<TracsactionField>
-				{props.users.map((item) => {
+				{spentUsers.map((user) => {
 					return (
-						<UserAmountRow key={'1'}>
-							<UserName>{item.name}</UserName>
+						<UserAmountRow key={user.id}>
+							<UserName>{user.name}</UserName>
 							<PayerInput
 								min={0}
+								value={user.amount}
 								type="number"
 								id="amount-input"
 								className="amount-input"
+								data-id={user.id}
+								onChange={changeSpentAmount}
 							></PayerInput>
 						</UserAmountRow>
 					);
@@ -97,7 +169,7 @@ function NewTransactionWidget(props) {
 
 			<ButtonsContainer>
 				<CancelButton>Cancel</CancelButton>
-				<AddButton>Add</AddButton>
+				<AddButton onClick={createObj}>Add</AddButton>
 			</ButtonsContainer>
 		</NewTransactionBlock>
 	);
